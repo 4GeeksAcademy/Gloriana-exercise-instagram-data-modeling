baf3d0b8d4086366cc4df5fec5a9d514b4836533
Glori <128950766+GlorianaZelaya@users.noreply.github.com>
2023-08-04 01:16:25 +0000

    Listo

--- src/models.py
+++ src/models.py
@@ -7,14 +7,49 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    ID = Column(Integer, primary_key=True)
+    username = Column(String(30))
+    firstname = Column(String(50))
+    lastname = Column(String(50))
+    email = Column(String(50), nullable=False, unique=True)
 
-class Address(Base):
+class Follower(Base):
+    __tablename__ = 'followers'
+    ID = Column(Integer, primary_key=True)
+    user_from_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship('User', back_populates='followers')
+    user_to_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='followers') 
+    
+class Post(Base):
+    __tablename__ = 'post'
+    ID = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship('User', back_populates='post')
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    ID = Column(Integer, primary_key=True)
+    comment_text = Column(String(2000))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('Post', back_populates='comment')
+    author_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('User', back_populates='comment')
+
+class Media(Base):
+    __tablename__ = 'media'
+    ID = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    url = Column(String(1000))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship('Post', back_populates='media')
+
+
+""" class Address(Base):
     __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
@@ -26,7 +61,7 @@ class Address(Base):
     person = relationship(Person)
 
     def to_dict(self):
-        return {}
+        return {} """
 
 ## Draw from SQLAlchemy base
 try:
